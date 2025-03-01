1ccf3a79056b0664f069971927c16a88cee33128
Nitipong Kadsritalee <nitipongkadsritalee01@gmail.com>
2025-03-01 20:14:36 +0700

    Add urls and views + register form

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path('', views.index, name='main_page'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+]
